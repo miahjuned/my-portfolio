e821afa5e047eee747574a4ff53272fe341105a9
zunead <zuned1996@outlook.com>
2021-06-10 23:38:43 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -6,15 +6,13 @@ import {
   Route,
 } from "react-router-dom";
 import About from './Component/About/About/About';
-import NavigastionBar from './Component/Home/Navbar/NavigastionBar';
-// import FooterBar from './Component/Home/Footer/Footer';
 import Blog from './Component/Home/Blog/Blog';
 import Work from './Component/Home/Work/Work';
 import Contract from './Component/Contract/Contract';
 import NoMatch from './Component/404/noMatch';
 import SmoothScroll from './Component/Home/SmoothScroll/SmoothScroll';
 import Deshboard from './Component/Home/Deshboard/Deshboard/Deshboard';
-import Sidebar_home from './Component/Home/Deshboard/ApointmentByDate/Sidebar_page/Sidebar_home';
+import SidebarHome from './Component/Home/Deshboard/ApointmentByDate/Sidebar_page/Sidebar_home';
 import SidebarBlog from './Component/Home/Deshboard/ApointmentByDate/Sidebar_page/SidebarBlog';
 import Sidebarproject from './Component/Home/Deshboard/ApointmentByDate/Sidebar_page/Sidebarproject';
 
@@ -22,7 +20,6 @@ function App() {
   return (
     <Router>
       <SmoothScroll></SmoothScroll>
-      {/* <NavigastionBar/> */}
       <Switch>
         <Route exact path="/">
           <Home/>
@@ -33,7 +30,6 @@ function App() {
         </Route>
 
         <Route path="/blog">
-          {/* <NavigastionBar/> */}
           <Blog></Blog>
         </Route>
 
@@ -42,7 +38,7 @@ function App() {
         </Route>
 
         <Route path="/sidebarhome">
-          <Sidebar_home></Sidebar_home>
+          <SidebarHome></SidebarHome>
         </Route>
 
         <Route path="/sidebarblog">
@@ -70,7 +66,6 @@ function App() {
         </Route>
 
       </Switch>
-      {/* <FooterBar/> */}
     </Router>
   );
 }
--- src/Component/About/About/About.js
+++ src/Component/About/About/About.js
@@ -3,22 +3,28 @@ import { Container } from 'react-bootstrap';
 import Education from '../Education/Education';
 import MyInfo from '../My_Info/My_Info';
 import Skill from '../Skill/Skill';
+import NavigastionBar from '../../Home/Navbar/NavigastionBar';
 import './About.css';
+import Footer from '../../Home/Footer/Footer';
 
 const About = () => {
     return (
-        <Container>
-            <section>
-                <div className="section-inner about-me">
-                    <div className="page-header">
-                        <h2>About Me</h2>
+        <div>
+            <NavigastionBar></NavigastionBar>
+            <Container>
+                <section>
+                    <div className="section-inner about-me">
+                        <div className="page-header">
+                            <h2>About Me</h2>
+                        </div>
                     </div>
-                </div>
-            </section>
-            <MyInfo></MyInfo>
-            <Skill></Skill>
-            <Education></Education>
-        </Container>
+                </section>
+                <MyInfo></MyInfo>
+                <Skill></Skill>
+                <Education></Education>
+            </Container>
+            <Footer></Footer>
+        </div>
     );
 };
 
