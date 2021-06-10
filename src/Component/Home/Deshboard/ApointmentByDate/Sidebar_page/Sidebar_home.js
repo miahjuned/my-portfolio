7a2f2328b149f39d67f69eabd246108810c06317
zunead <zuned1996@outlook.com>
2021-06-10 23:53:40 +0600

    updated

--- src/Component/Home/Deshboard/ApointmentByDate/Sidebar_page/Sidebar_home.js
+++ src/Component/Home/Deshboard/ApointmentByDate/Sidebar_page/Sidebar_home.js
@@ -5,18 +5,16 @@ import Sidebar from '../../Sidebar/Sidebar';
 
 const Sidebar_home = () => {
     return (
-        <div>
-            <Row>
-                <Col md={3}>
-                    <div className="">
-                        <Sidebar></Sidebar>
-                    </div>
-                </Col>
-                <Col md={9}>
-                    <Home></Home>
-                </Col>
-            </Row>
-        </div>
+        <Row>
+            <Col md={3}>
+                <div className="">
+                    <Sidebar></Sidebar>
+                </div>
+            </Col>
+            <Col md={9}>
+                <Home></Home>
+            </Col>
+        </Row>
     );
 };
 
--- src/Component/Home/Deshboard/Deshboard/Deshboard.js
+++ src/Component/Home/Deshboard/Deshboard/Deshboard.js
@@ -1,30 +1,28 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import ApointmentByDate from '../ApointmentByDate/ApointmentByDate';
 import Sidebar from '../Sidebar/Sidebar';
 import './Deshboard.css';
 const Deshboard = () => {
     return (
-        <div>
-            <Row className="dashboard">
-                <Col md={3}>
-                    <div  >
-                        <Sidebar></Sidebar>
-                    </div>
-                </Col>
-                <Col md={5} >
-                    <div className="" >
-                        <h1>Caendar</h1>
-                    </div>
-                </Col>
-                <Col  md={4} >
-                    <div className="" >
-                        <ApointmentByDate></ApointmentByDate>
-                    </div>
-                </Col>
-                
+        <Row className="dashboard">
+            <Col md={3}>
+                <div  >
+                    <Sidebar></Sidebar>
+                </div>
+            </Col>
+            <Col md={5} >
+                <div className="" >
+                    <h1>Caendar</h1>
+                </div>
+            </Col>
+            <Col  md={4} >
+                <div className="" >
+                    <ApointmentByDate></ApointmentByDate>
+                </div>
+            </Col>
+            
             </Row>
-        </div>
     );
 };
 
